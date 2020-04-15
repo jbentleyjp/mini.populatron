0bb16fc94ce3196d42112214c811179fc94526f7
Jeff Bentley <jbentley449@gmail.com>
2020-04-16 01:13:01 +0900

    first commit, not able to progress muchat the moment

--- populatron.js
+++ populatron.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    onFinished(
+      fs.readFile(
+        "cities.csv",
+        "utf8",
+        (error, result) => console.log(error, result)
+        //use a callback, promise, or async to delay the function until the data is processed
+      )
+    );
+  },
 };
